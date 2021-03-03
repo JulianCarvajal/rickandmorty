0181c2be8db27c9fdd12c47c29e1d8851d1d93dc
Julian <juli19cam@gmail.com>
2021-03-03 11:37:26 -0500

    Async function to render templates according to the hash. New event listener to reload the routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Character from '../pages/Character';
 import Error from '../pages/Error404';
 import getHash from '../utils/getHash';
 import resolveRoutes from '../utils/resolveRoutes';
+import Error404 from '../pages/Error404';
 
 const routes = {
     '/': Home,
@@ -16,6 +17,11 @@ const router = async () => {
     const content = null || document.getElementById('content');
 
     header.innerHTML = await Header();
+    let hash = getHash();
+    let route = await resolveRoutes(hash);
+    let render = routes[route] ? routes[route] : Error404;
+    content.innerHTML = await render();
+
 }
 
 export default router;
