68cc4a5f44835db3201cfd21b9b10ee94415224d
Julian <juli19cam@gmail.com>
2021-03-04 16:15:00 -0500

    Just change the logo and added a burger menu button ... still don't know why but maybe this will be util. I have to fix the burger button

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,10 +3,14 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/">
-                        100tifi.co
-                    </a>
+                    <figure class="Header-logo">
+                        <img src="https://upload.wikimedia.org/wikipedia/en/c/c8/Rick_and_Morty_logo.png" alt="logo">
+                    </figure>
                 </h1>
+            </div>
+
+            <div class="burger-menu" id="menu_button">
+                <i class="fas fa-bars"></i>
             </div>
 
             <div class="Header-nav">
