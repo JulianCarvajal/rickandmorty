7136883fb7a7f5752360bbd90cbee6320aa103ee
Julian <juli19cam@gmail.com>
2021-05-07 23:00:17 -0500

    New file for search item, the index.html file was modified to add the required elements for the search item

---
+++ src/templates/Search.js
@@ -0,0 +1,10 @@
+const Search = () => {
+    const view = `
+        <div class="search">
+            <input type="search" class="search--bar" placeholder="Busca a tu personaje favorito" />
+        </div>
+    `;
+    return view;
+}
+
+export default Search;
