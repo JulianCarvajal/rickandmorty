e79ea37b77a83dafef3018f52d30d4a2197cf921
Julian <juli19cam@gmail.com>
2021-05-07 23:12:34 -0500

    Added the Search template to the index.js file to render the content on the main page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import Header from '../templates/Header';
+import Search from '../templates/Search';
 import Home from '../pages/Home';
 import Pages from '../pages/Pages';
 import Character from '../pages/Character';
@@ -16,9 +17,11 @@ const routes = {
 
 const router = async () => {
     const header = null || document.getElementById('header');
+    const search = null || document.getElementById('search');
     const content = null || document.getElementById('content');
 
     header.innerHTML = await Header();
+    search.innerHTML = await Search();
     let hash = getHash();
     let route = await resolveRoutes(hash);
     let render = routes[route] ? routes[route] : Error404;
