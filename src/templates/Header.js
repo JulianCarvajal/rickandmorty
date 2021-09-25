d0ba6bd8a46fe0fa9ee86ac3fdd4bf87291a2be4
Julian <juli19cam@gmail.com>
2021-09-24 22:08:26 -0500

    Improving the last design details, make the characters view responsive and adding a good colour for the character information

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -13,17 +13,15 @@ const Header = () => {
         </div>
 
         <section class="Welcome">
-            <div class="Welcome--content">
-                <h2 class="Welcome--title">
-                    Bienvenido!!!
-                </h2>
-                <p class="Welcome--text">
-                    Esta es una web para los amantes de Rick y Morty o aquellos que simplemente quieren conocer más acerca de este interesante multiverso lleno de comedia, paradojas y sobre todo mucha ciencia.
-                </p>
-                <p class="Welcome--text second">
-                    Aquí encontrarás una lista completa de todos los personajes de la serie y podrás ver sus principales características, además podrás buscar a tu personaje favorito por su nombre y conocer todas sus versiones en el multiverso y su estado actual. Para hacer esto utilicé la API gratuita de Rick and Morty, podrás leer más sobre ella en <a class="Welcome--link" target="_blank" href="https://rickandmortyapi.com/">Rick and Morty API</a>
-                </p>
-            </div>
+            <h2 class="Welcome--title">
+                Bienvenido!!!
+            </h2>
+            <p class="Welcome--text">
+                Esta es una web para los amantes de Rick y Morty o aquellos que simplemente quieren conocer más acerca de este interesante multiverso lleno de comedia, paradojas y sobre todo mucha ciencia.
+            </p>
+            <p class="Welcome--text second">
+                Aquí encontrarás una lista completa de todos los personajes de la serie y podrás ver sus principales características, además podrás buscar a tu personaje favorito por su nombre y conocer todas sus versiones en el multiverso y su estado actual. Para hacer esto utilicé la API gratuita de Rick and Morty, podrás leer más sobre ella en <a class="Welcome--link" target="_blank" href="https://rickandmortyapi.com/">Rick and Morty API</a>
+            </p>
         </section>
     `;
     return view;
